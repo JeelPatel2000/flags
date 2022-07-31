cea0feaab7653fa97229a06d9bc13c37c1da26e7
Jeel Patel <jeel.patel1111@gmail.com>
2022-08-01 02:34:58 +0800

    prod api url

--- apps/frontend/src/config.ts
+++ apps/frontend/src/config.ts
@@ -1,7 +1,6 @@
 const config = {
     DEV_API_URL: "http://localhost:4300",
-    PROD_API_URL:
-        "http://ec2-35-183-130-142.ca-central-1.compute.amazonaws.com:4300",
+    PROD_API_URL: "https://flags.jeel.dev",
 };
 
 export default config;
