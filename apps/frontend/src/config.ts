4e4bc227c79fcdc1d2235e536a3cb0292043c0e0
Rushil Patel <rushilp2311@gmail.com>
2022-07-31 12:13:16 -0400

    update config file

--- apps/frontend/src/config.ts
+++ apps/frontend/src/config.ts
@@ -1,7 +1,7 @@
 const config = {
     DEV_API_URL: "http://localhost:4300",
     PROD_API_URL:
-        "http://ec2-99-79-37-226.ca-central-1.compute.amazonaws.com:4300",
+        " http://ec2-35-183-130-142.ca-central-1.compute.amazonaws.com:4300",
 };
 
 export default config;
